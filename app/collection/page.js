3f74c68f6c37c31168e21d8b06768e17979a5752
Nahteee <ethantkz2522@gmail.com>
2023-10-14 18:54:44 +0800

    Collection

--- app/collection/page.js
+++ app/collection/page.js
@@ -1,11 +1,41 @@
 import { Container } from "@mui/material"
 
-export default function CollectionPage () {
-    return (
-        <>
-            <Container maxWidth="lg" sx={{marginTop: 3}}>
-                <h1>My Waifu Collection</h1>
-            </Container>
-        </>
-    )
+
+// import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import CustomItem from '@/components/Misc/CustomItem';
+import Image from 'next/image'
+
+
+
+
+export default function BasicGrid() {
+  return (
+    <Container maxWidth="lg" sx={{marginTop: 3}}>
+        <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={6}>
+          <CustomItem>xs=4</CustomItem>
+        </Grid>
+        <Grid xs={6}>
+          <CustomItem>xs=4</CustomItem>
+        </Grid>
+        <Grid xs={6}>
+          <CustomItem>xs=4</CustomItem>
+        </Grid>
+        <Grid xs={6}>
+          <CustomItem>xs=4</CustomItem>
+        </Grid>
+      </Grid>
+    </Box>
+    <Image
+      src="/profile.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+    </Container>
+    
+  );
 }

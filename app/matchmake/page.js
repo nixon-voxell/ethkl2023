367797ca642da40ecf8ba9efdb1fd298c3e9925c
Yuan <waiiyuanchai21@gmail.com>
2023-10-14 19:50:14 +0800

    ui changes

--- app/matchmake/page.js
+++ app/matchmake/page.js
@@ -4,6 +4,8 @@ import * as React from 'react'
 import { Container, Button } from "@mui/material"
 import PageTitle from "@/components/Paging/PageTitle"
 import BasicModal from '@/components/Common/NftModal'
+import Orders from '@/components/Dashboard/Orders'
+import Paper from '@mui/material/Paper';
 
 export default function MatchmakePage () {
 
@@ -17,6 +19,10 @@ export default function MatchmakePage () {
                 <PageTitle title="Matchmake" />
                 <Button variant="contained" sx={{marginTop: 5}} onClick={handleOpen}>Matchmake Now</Button>
                 <BasicModal open={open} onClose={handleClose} />
+
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', marginTop: 5 }}>
+                  <Orders />
+                </Paper>
             </Container>
         </>
     )
--- app/page.js
+++ app/page.js
@@ -1,17 +1,74 @@
-import { Container, Divider } from '@mui/material';
-import { Typography } from '@mui/material';
-import PageTitle from '@/components/Paging/PageTitle';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-// import { ethers } from 'ethers';
-// import { abi } from '@/abi/abi';
+"use client";
+
+import { Container, Divider } from "@mui/material";
+import { useState } from "react";
+import { Typography } from "@mui/material";
+import PageTitle from "@/components/Paging/PageTitle";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Dashboard from "@/components/Dashboard/Dashboard";
+import { ethers } from "ethers";
+import { abi } from "@/components/Abi/abi";
 
 export default function Home() {
+  const [name, setName] = useState();
+  const [symbol, setSymbol] = useState();
+  const [balance, setBalance] = useState();
+
+  const getBlock = async () => {
+    const wallet = new ethers.BrowserProvider(window.ethereum); // Referring to metamask
+    const block = await wallet.getBlockNumber();
+
+    // console.log(block)
+
+    // Get connected address
+    const address = await wallet.getSigner();
+
+    // Define a new contact in ethers
+    const token = new ethers.Contract(
+      `0xc170bd5653B0d499eE2cAa700E4338B7549424eD`,
+      abi,
+      address
+    );
+
+    const name = await token.name();
+    setName(name);
+
+    const symbol = await token.symbol();
+    setSymbol(symbol);
+
+    // get connected address balance
+
+    const balance = await token.balanceOf(address.address);
+    setBalance(balance);
+  };
+
+  const mintToken = async () => {
+    const wallet = new ethers.BrowserProvider(window.ethereum); // Referring to metamask
+    const block = await wallet.getBlockNumber();
+
+    // console.log(block)
+
+    // Get connected address
+    const address = await wallet.getSigner();
+
+    const token = new ethers.Contract(
+      `0xc170bd5653B0d499eE2cAa700E4338B7549424eD`,
+      abi,
+      address
+    );
+
+    await token.mint(address.address, ethers.parseEther("1000"));
+  };
+
+  getBlock();
+
   return (
-    <Container maxWidth="lg" sx={{marginTop: 3, fontWeight: 700}}>
-      <PageTitle title="Main Page" />
+    <Container maxWidth="lg" sx={{ marginTop: 3, fontWeight: 700 }}>
+      <PageTitle title={`gm ${name}  😘`} />
+      <Dashboard balance={balance}/>
     </Container>
-  )
+  );
 }

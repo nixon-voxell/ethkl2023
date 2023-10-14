d2f0110ebced6fffe3d12cab3e3c2fa97531c8d9
Yuan <waiiyuanchai21@gmail.com>
2023-10-14 20:00:29 +0800

    pagetitle for collection

--- app/collection/page.js
+++ app/collection/page.js
@@ -6,11 +6,13 @@ import Grid from "@mui/material/Unstable_Grid2";
 import CustomItem from "@/components/Misc/CustomItem";
 import Image from "next/image";
 import { Margin } from "@mui/icons-material";
+import PageTitle from "@/components/Paging/PageTitle";
 
 export default function BasicGrid() {
   return (
     <Container maxWidth="lg" sx={{ marginTop: 3 }}>
-      <Box sx={{ flexGrow: 1 }}>
+      <PageTitle title="My Waifu 🥰" />
+      <Box sx={{ flexGrow: 1, marginTop: 3 }}>
         <Grid container spacing={2}>
           <Grid xs={4} sx={{textAlign: "center"}}>
             <Image

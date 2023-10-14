cf98efe4674992a0efa64295c8a458bd36ba5d13
Nahteee <ethantkz2522@gmail.com>
2023-10-15 00:01:55 +0800

    Voting main page done

--- app/collection/page.js
+++ app/collection/page.js
@@ -6,77 +6,60 @@ import Grid from "@mui/material/Unstable_Grid2";
 import CustomItem from "@/components/Misc/CustomItem";
 import Image from "next/image";
 import { Margin } from "@mui/icons-material";
+import CustomImage from "@/components/Misc/CustomImage";
 
 export default function BasicGrid() {
   return (
     <Container maxWidth="lg" sx={{ marginTop: 3 }}>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid xs={4} sx={{textAlign: "center"}}>
-            <Image
-              src="/Waifus/1.png"
-              width={250}
-              height={250}
-              alt="Picture of the author"
+          <Grid xs={4} sx={{ textAlign: "center" }}>
+            <CustomImage
+              imagePath="/Waifus/1.png"
+              title="Best Waifu"
+              matchCount="15"
+              winCount="6"
             />
-            <h2>Best Waifu</h2>
-            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
-            <p>Win History: 6</p>
           </Grid>
-          <Grid xs={4} sx={{textAlign: "center"}}>
-            <Image
-              src="/Waifus/2.png"
-              width={250}
-              height={250}
-              alt="Picture of the author"
+          <Grid xs={4} sx={{ textAlign: "center" }}>
+            <CustomImage
+              imagePath="/Waifus/2.png"
+              title="Best Waifu"
+              matchCount="15"
+              winCount="6"
             />
-            <h2>Best Waifu</h2>
-            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
-            <p>Win History: 2</p>
           </Grid>
-          <Grid xs={4} sx={{textAlign: "center"}}>
-            <Image
-              src="/Waifus/3.png"
-              width={250}
-              height={250}
-              alt="Picture of the author"
+          <Grid xs={4} sx={{ textAlign: "center" }}>
+            <CustomImage
+              imagePath="/Waifus/3.png"
+              title="Best Waifu"
+              matchCount="15"
+              winCount="6"
             />
-            <h2>Best Waifu</h2>
-            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
-            <p>Win History: 2</p>
           </Grid>
-          <Grid xs={4} sx={{textAlign: "center"}}>
-            <Image
-              src="/Waifus/4.png"
-              width={250}
-              height={250}
-              alt="Picture of the author"
+          <Grid xs={4} sx={{ textAlign: "center" }}>
+            <CustomImage
+              imagePath="/Waifus/4.png"
+              title="Best Waifu"
+              matchCount="15"
+              winCount="6"
             />
-            <h2>Best Waifu</h2>
-            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
-            <p>Win History: 4</p>
           </Grid>
-          <Grid xs={4} sx={{textAlign: "center"}}>
-            <Image
-              src="/Waifus/5.png"
-              width={250}
-              height={250}
-              alt="Picture of the author"
+          <Grid xs={4} sx={{ textAlign: "center" }}>
+            <CustomImage
+              imagePath="/Waifus/5.png"
+              title="Best Waifu"
+              matchCount="15"
+              winCount="6"
             />
-            <h2>Best Waifu</h2>
-            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
-            <p>Win History: 0</p>
           </Grid>
-          <Grid xs={4} sx={{textAlign: "center"}}>
-            <Image
-              src="/Waifus/6.png"
-              width={250}
-              height={250}
-              alt="Picture of the author"
+          <Grid xs={4} sx={{ textAlign: "center" }}>
+            <CustomImage
+              imagePath="/Waifus/6.png"
+              title="Best Waifu"
+              matchCount="15"
+              winCount="6"
             />
-            <h2>Best Waifu</h2>
-            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
-            <p>Win History: </p>
           </Grid>
         </Grid>
       </Box>
--- app/voting/page.js
+++ app/voting/page.js
@@ -1,29 +1,98 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { Container, Button } from "@mui/material"
-import PageTitle from "@/components/Paging/PageTitle"
-import BasicModal from '@/components/Common/NftModal'
-import Orders from '@/components/Dashboard/Orders'
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import { Container, Button } from "@mui/material";
+import PageTitle from "@/components/Paging/PageTitle";
+import BasicModal from "@/components/Common/NftModal";
+import Orders from "@/components/Dashboard/Orders";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { Divider, CircularProgress } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import TextField from "@mui/material/TextField";
 
-export default function StorePage () {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  //   border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  height: 600,
+};
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    
-    return (
-        <>
-            <Container maxWidth="lg" sx={{marginTop: 3}}>
-                <PageTitle title="Matchmake" />
-                <Button variant="contained" sx={{marginTop: 5}} onClick={handleOpen}>Matchmake Now</Button>
-                <BasicModal open={open} onClose={handleClose} />
+export default function StorePage() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', marginTop: 5 }}>
-                  <Orders />
-                </Paper>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <Container maxWidth="lg" sx={{ marginTop: 3 }}>
+        <PageTitle title="Vote for the best Waifu!" />
+        <h2>Ether Balance: 10 Eth</h2>
+
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch",  marginTop: 2 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="outlined-basic" label="Bet Amount" variant="outlined" />
+        </Box>
+        <Button variant="contained" sx={{ marginTop: 2 }} onClick={handleOpen}>
+          Enter Voting Pool
+        </Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="child-modal-title"
+          aria-describedby="child-modal-description"
+        >
+          <Box sx={{ ...style, width: 500, height: 100 }}>
+            <Grid container spacing={2}>
+              <Grid
+                xs={4}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Typography
+                  id="modal-modal-title"
+                  sx={{ fontWeight: 700 }}
+                  variant="h6"
+                  component="h2"
+                  gutterBottom
+                >
+                  Joining ❤️
+                </Typography>
+              </Grid>
+              <Grid
+                xs={1}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <CircularProgress size="1.2rem" />
+              </Grid>
+            </Grid>
+          </Box>
+        </Modal>
+
+        <Paper
+          sx={{ p: 2, display: "flex", flexDirection: "column", marginTop: 5 }}
+        >
+          <Orders />
+        </Paper>
+      </Container>
+    </>
+  );
 }

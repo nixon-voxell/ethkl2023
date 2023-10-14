c771a98ce16c08f2fb0bb525097ce667ff407e6f
Nahteee <ethantkz2522@gmail.com>
2023-10-14 19:33:14 +0800

    Waifus added to collection

--- app/collection/page.js
+++ app/collection/page.js
@@ -1,41 +1,85 @@
-import { Container } from "@mui/material"
-
+import { Container } from "@mui/material";
 
 // import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import CustomItem from '@/components/Misc/CustomItem';
-import Image from 'next/image'
-
-
-
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import CustomItem from "@/components/Misc/CustomItem";
+import Image from "next/image";
+import { Margin } from "@mui/icons-material";
 
 export default function BasicGrid() {
   return (
-    <Container maxWidth="lg" sx={{marginTop: 3}}>
-        <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid xs={6}>
-          <CustomItem>xs=4</CustomItem>
+    <Container maxWidth="lg" sx={{ marginTop: 3 }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={4} sx={{textAlign: "center"}}>
+            <Image
+              src="/Waifus/1.png"
+              width={250}
+              height={250}
+              alt="Picture of the author"
+            />
+            <h2>Best Waifu</h2>
+            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
+            <p>Win History: 6</p>
+          </Grid>
+          <Grid xs={4} sx={{textAlign: "center"}}>
+            <Image
+              src="/Waifus/2.png"
+              width={250}
+              height={250}
+              alt="Picture of the author"
+            />
+            <h2>Best Waifu</h2>
+            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
+            <p>Win History: 2</p>
+          </Grid>
+          <Grid xs={4} sx={{textAlign: "center"}}>
+            <Image
+              src="/Waifus/3.png"
+              width={250}
+              height={250}
+              alt="Picture of the author"
+            />
+            <h2>Best Waifu</h2>
+            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
+            <p>Win History: 2</p>
+          </Grid>
+          <Grid xs={4} sx={{textAlign: "center"}}>
+            <Image
+              src="/Waifus/4.png"
+              width={250}
+              height={250}
+              alt="Picture of the author"
+            />
+            <h2>Best Waifu</h2>
+            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
+            <p>Win History: 4</p>
+          </Grid>
+          <Grid xs={4} sx={{textAlign: "center"}}>
+            <Image
+              src="/Waifus/5.png"
+              width={250}
+              height={250}
+              alt="Picture of the author"
+            />
+            <h2>Best Waifu</h2>
+            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
+            <p>Win History: 0</p>
+          </Grid>
+          <Grid xs={4} sx={{textAlign: "center"}}>
+            <Image
+              src="/Waifus/6.png"
+              width={250}
+              height={250}
+              alt="Picture of the author"
+            />
+            <h2>Best Waifu</h2>
+            <p style={{ marginTop: '8px' }}>Matches Played: 15</p>
+            <p>Win History: </p>
+          </Grid>
         </Grid>
-        <Grid xs={6}>
-          <CustomItem>xs=4</CustomItem>
-        </Grid>
-        <Grid xs={6}>
-          <CustomItem>xs=4</CustomItem>
-        </Grid>
-        <Grid xs={6}>
-          <CustomItem>xs=4</CustomItem>
-        </Grid>
-      </Grid>
-    </Box>
-    <Image
-      src="/profile.png"
-      width={500}
-      height={500}
-      alt="Picture of the author"
-    />
+      </Box>
     </Container>
-    
   );
 }
